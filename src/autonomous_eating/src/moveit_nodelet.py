be88c80bdd6326c3977769bca7941ca37ce6d241
ubuntu <vm@gmail.com>
2020-04-01 00:15:08 +0200

    TransformV1

--- src/autonomous_eating/src/moveit_nodelet.py
+++ src/autonomous_eating/src/moveit_nodelet.py
@@ -5,84 +5,95 @@ import rospy
 import moveit_commander
 import moveit_msgs.msg
 import geometry_msgs.msg
+import tf2_ros
+from tf.transformations import quaternion_from_euler
 from math import pi
 from std_msgs.msg import String
 from moveit_commander.conversions import pose_to_list
 
-moveit_commander.roscpp_initialize(sys.argv)
-rospy.init_node('move_group_python_interface_tutorial',
-                log_level=True)
-
-robot = moveit_commander.RobotCommander()
-
-scene = moveit_commander.PlanningSceneInterface()
-
+robotName = "j2n6s300"
 group_name = "arm"
-group = moveit_commander.MoveGroupCommander(group_name)
+rootFrame = 'root'
+cameraAttachFrame = 'j2n6s300_link_6'
+cameraNameFrame = 'realsense'
+#x, y, z, roll, yaw, pitch
+cameraTransformation = [0, 0, 0.2, 0, 0, 0]
 
-display_trajectory_publisher = rospy.Publisher('/move_group/display_planned_path',
+camera_Message = geometry_msgs.msg.Transform()
+
+camera_Message.transform.translation.x = cameraTransformation[0]
+camera_Message.transform.translation.y = cameraTransformation[1]
+camera_Message.transform.translation.z = cameraTransformation[2]
+
+quad = quaternion_from_euler(cameraTransformation[3], cameraTransformation[4], cameraTransformation[5])
+
+camera_Message.transform.rotation.x = quad[0]
+camera_Message.transform.rotation.y = quad[1]
+camera_Message.transform.rotation.z = quad[2]
+camera_Message.transform.rotation.w = quad[3]
+
+if __name__ == '__main__':
+    moveit_commander.roscpp_initialize(sys.argv)
+    rospy.init_node('move_group_python_interface_tutorial')
+
+    display_trajectory_publisher = rospy.Publisher('/move_group/display_planned_path',
                                             moveit_msgs.msg.DisplayTrajectory,
                                             queue_size=20)
 
-# We can get the name of the reference frame for this robot:
-planning_frame = group.get_planning_frame()
-print "============ Reference frame: %s" % planning_frame
+    rate = rospy.Rate(10.0)
 
-# We can also print the name of the end-effector link for this group:
-eef_link = group.get_end_effector_link()
-print "============ End effector: %s" % eef_link
+    group = moveit_commander.MoveGroupCommander(group_name)
+    robot = moveit_commander.RobotCommander()
 
-# We can get a list of all the groups in the robot:
-group_names = robot.get_group_names()
-print "============ Robot Groups:", robot.get_group_names()
+    planning_frame = group.get_planning_frame()
+    eef_link = group.get_end_effector_link()
+    group_names = robot.get_group_names()
 
-# Sometimes for debugging it is useful to print the entire state of the
-# robot:
-print "============ Printing robot state"
-print robot.get_current_state()
-print ""
+    tfBuffer = tf2_ros.Buffer()
+    listener = tf2_ros.TransformListener(tfBuffer)
+    camera_Broadcaster = tf2_ros.TransformBroadcaster()
 
-pose_goal = geometry_msgs.msg.Pose()
-pose_goal.orientation.w = 1.0
-pose_goal.position.x = 0.4
-pose_goal.position.y = 0.1
-pose_goal.position.z = 0.4
-group.set_pose_target(pose_goal)
+    # robot:
+    print "============ Printing robot state"
+    print robot.get_current_state()
+    print ""
 
-plan = group.go(wait=True)
-# Calling `stop()` ensures that there is no residual movement
-group.stop()
-# It is always good to clear your targets after planning with poses.
-# Note: there is no equivalent function for clear_joint_value_targets()
-group.clear_pose_targets()
+    plan = group.go(wait=True)
+    group.stop()
+    group.clear_pose_targets()
 
-waypoints = []
+    waypoints = []
 
-scale = 0.5
+    wpose = group.get_current_pose().pose
+    wpose.position.z = 0.2
+    wpose.position.y = 0.6
+    waypoints.append(copy.deepcopy(wpose))
 
-wpose = group.get_current_pose().pose
-wpose.position.z -= scale * 0.1  # First move up (z)
-wpose.position.y += scale * 0.2  # and sideways (y)
-waypoints.append(copy.deepcopy(wpose))
+    wpose.position.x = 0.6
+    waypoints.append(copy.deepcopy(wpose))
 
-wpose.position.x += scale * 0.1  # Second move forward/backwards in (x)
-waypoints.append(copy.deepcopy(wpose))
+    wpose.position.y = 0.5
+    waypoints.append(copy.deepcopy(wpose))
 
-wpose.position.y -= scale * 0.1  # Third move sideways (y)
-waypoints.append(copy.deepcopy(wpose))
+    #(plan, fraction) = group.compute_cartesian_path(
+    #                                waypoints,   # waypoints to follow
+    #                                0.01,        # eef_step
+    #                                0.0)         # jump_threshold
 
-# We want the Cartesian path to be interpolated at a resolution of 1 cm
-# which is why we will specify 0.01 as the eef_step in Cartesian
-# translation.  We will disable the jump threshold by setting it to 0.0 disabling:
-(plan, fraction) = group.compute_cartesian_path(
-                                   waypoints,   # waypoints to follow
-                                   0.01,        # eef_step
-                                   0.0)         # jump_threshold
+    #display_trajectory = moveit_msgs.msg.DisplayTrajectory()
+    #display_trajectory.trajectory_start = robot.get_current_state()
+    #display_trajectory.trajectory.append(plan)
+    # Publish
+    #display_trajectory_publisher.publish(display_trajectory)
 
-display_trajectory = moveit_msgs.msg.DisplayTrajectory()
-display_trajectory.trajectory_start = robot.get_current_state()
-display_trajectory.trajectory.append(plan)
-# Publish
-display_trajectory_publisher.publish(display_trajectory);
+    #group.execute(plan, wait=True)
 
-group.execute(plan, wait=True)
+    while not rospy.is_shutdown():
+        rospy.spin()
+        #try:
+            #First frame, end frame
+        #    trans = tfBuffer.lookup_transform(rootFrame, cameraAttachFrame, rospy.Time(0))
+        #except (tf2_ros.LookupException, tf2_ros.ConnectivityException, tf2_ros.ExtrapolationException):
+        #    rate.sleep()
+        #    rospy.logerr("Failed")
+        #    continue
