2d2595e82967394473c1041e85dc47eb46867567
ubuntu <andersclement96@gmail.com>
2020-04-03 14:32:17 +0200

    now subscribes to ros topics

--- src/autonomous_eating/scripts/gui.py
+++ src/autonomous_eating/scripts/gui.py
@@ -6,10 +6,21 @@ from ttk import *
 from PIL import ImageTk, Image
 
 import rospkg
+import rospy
+from std_msgs.msg import String
+from autonomous_eating.msg import gui_status
+from autonomous_eating.msg import gui_mode
 
 pkgPath = rospkg.RosPack().get_path('autonomous_eating')
 
-class App():
+class GUIApp():
+  def gui_mode_callback(self, data):
+    self.updateAcitvationBar(data.selectness)
+    self.updateItciImg(self.frame, data.x, data.y)
+
+  def gui_status_callback(self, data):
+    status = (data.jaco_status, data.camera_status, data.main_status, data.moving_status)
+    self.updateStatusText(status)
 
   def updateItciImg(self, frame, x=25,y=25):
     self.itciCanvas.create_image(0,0, anchor=NW, image=self.itciBaseImg, state=NORMAL)
@@ -25,6 +36,7 @@ class App():
 
   def updateStatusText(self, status):
     self.text.configure(state="normal")
+    self.text.delete(1.0, END)
     self.text.insert(END, "JACO Connect:  " + status[0] + "\n")
     self.text.insert(END, "Camera status:   " + status[1] + "\n")
     self.text.insert(END, "System status:    " + status[2] + "\n")
@@ -32,11 +44,11 @@ class App():
     self.text.configure(state="disabled")
 
   def __init__(self, master):
-    frame = Frame(master)
-    frame.style = Style()
-    frame.pack()
+    self.frame = Frame(master)
+    self.frame.style = Style()
+    self.frame.pack()
     #('clam', 'alt', 'default', 'classic')
-    frame.style.theme_use("clam")
+    self.frame.style.theme_use("clam")
     master.title("Autonomous Eating")
 
     #label for status panels below it:
@@ -44,23 +56,30 @@ class App():
     #self.statusLabel.grid(column=0,row=0)
     
     #add info panel on the left, top part is status panel
-    self.text = Text(frame, font=("Arial", 12), width = 50)
+    self.text = Text(self.frame, font=("Arial", 12), width = 50)
     self.text.grid(column=0, row=0)
     self.updateStatusText(("Connected", "Disconnected", "OK", "False"))
 
     #adding itci image with red ellipse showing tongue position
     self.itciBaseImg = ImageTk.PhotoImage(Image.open(pkgPath + "/extra/figures/itci.png"))
-    self.itciCanvas = Canvas(frame, width = self.itciBaseImg.width(), height = self.itciBaseImg.height())
+    self.itciCanvas = Canvas(self.frame, width = self.itciBaseImg.width(), height = self.itciBaseImg.height())
     self.itciCanvas.grid(column=1, row=0, rowspan=2)
-    self.updateItciImg(frame) #center ellipse top-left at start
+    self.updateItciImg(self.frame) #center ellipse top-left at start
 
     #add activationbar showing 'activationness' of the itci
-    self.activationBar = Progressbar(frame, length=350)
+    self.activationBar = Progressbar(self.frame, length=350)
     self.activationBar.grid(column=1,row=2)
     self.updateAcitvationBar()
 
-#start of script:
-root = Tk()
-app = App(root)
-root.mainloop()
 
+#start of script, set up GUI:
+root = Tk()
+app = GUIApp(root)
+
+#init ros:
+rospy.init_node('display_node')
+rospy.Subscriber("/gui_status", gui_status , app.gui_status_callback)
+rospy.Subscriber("/gui_mode", gui_mode, app.gui_mode_callback)
+ 
+#loop using Tk instead of ros::spin, does the same thing, but keeps the GUI updating
+root.mainloop()
