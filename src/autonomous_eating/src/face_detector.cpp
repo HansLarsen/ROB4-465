15929f7f06e8e70e463e0319b15f6baf0d47b791
ubuntu <andersclement96@gmail.com>
2020-04-08 14:18:04 +0200

    ready to wrap in ROS

--- src/autonomous_eating/src/face_detector.cpp
+++ src/autonomous_eating/src/face_detector.cpp
@@ -1,84 +1,109 @@
 #include "ros/ros.h"
 #include "ros/time.h"
+#include "opencv2/core/version.hpp"
 #include "opencv2/objdetect/objdetect.hpp"
 #include "opencv2/highgui/highgui.hpp"
 #include "opencv2/imgproc/imgproc.hpp"
-//#include "opencv2/contrib/contrib.hpp"
+#include "opencv2/face.hpp"
 #include <iostream>
 using namespace std;
 using namespace cv;
 using namespace cv::face;
 
+struct faceData
+{
+  std::vector<Rect> faces;
+  vector<vector<Point2f>> landmarks;
+};
 
 class Face_worker
 {
-  Face_worker();
-  Face_worker(String face_cascade_filename, String landmark_model_filename);
-  ~Face_worker();
   CascadeClassifier face_cascade; 
   Ptr<Facemark> facemark;
 
-  void detectFace();
+public:
+  void init(String face_cascade_filename, String landmark_model_filename);
+  faceData detectFace(Mat frame);
+  ~Face_worker();
 };
 
-Face_worker::Face_worker(String face_cascade_filename, String landmark_model_filename)
+void Face_worker::init(String face_cascade_filename, String landmark_model_filename)
 {
   //load trained data to viola-jones detector
   face_cascade.load(face_cascade_filename);
   //ready facial landmark detection
   FacemarkLBF::Params params;
-  params.model_filename = "lbfmodel.yaml"; // the trained model
+  params.model_filename = landmark_model_filename; // the trained model
   facemark = FacemarkLBF::create(params);
   facemark->loadModel(params.model_filename);
 }
-void Face_worker::detectFace()
+
+faceData Face_worker::detectFace(Mat frame)
 {
-  Mat frame;
   if( frame.empty() )
   {
-    cout << "--(!) No captured frame -- Break!\n";
-    return;
+    ROS_ERROR_STREAM("EMPTY FACE FRAME, in: Face_worker::detectFace");// << "--(!) No captured frame -- Break!\n";
   }
 
-  //convert to gray
+  //convert img to gray
   Mat frame_gray;
   cvtColor( frame, frame_gray, COLOR_BGR2GRAY );
+
   //resize frame first for better performance:
   Size scale = frame_gray.size();
-  scale.width = scale.width/2;
-  scale.height = scale.height/2;
+  float to_scale = 300000.0/(frame.cols*frame.rows);
+  scale.width = scale.width*to_scale;
+  scale.height = scale.height*to_scale;
   resize(frame_gray,frame_gray,scale);
-  resize(frame, frame, scale);
-  //-- 3. Apply the classifier to the frame
 
+  //-- 3. Apply the classifier to the frame
   equalizeHist( frame_gray, frame_gray );
   //-- Detect faces
   std::vector<Rect> faces;
   face_cascade.detectMultiScale( frame_gray, faces );
 
+  //fit landmarks:
   vector<vector<Point2f>> landmarks;
   facemark->fit(frame,faces,landmarks);
 
+  //save in faceData struct to return it:
+  faceData data;
+  data.faces = faces;
+  data.landmarks = landmarks;
+  return data;
+
+
+  /* optionally draw faces, and landmarks on the frame
   for ( size_t i = 0; i < faces.size(); i++ )
   {
-      rectangle(frame, faces[i].tl(), faces[i].br(), Scalar(255,0,255), 4);
+      rectangle(frame_gray, faces[i].tl(), faces[i].br(), Scalar(255,0,255), 4);
       for(int j = 0; j < landmarks[i].size(); j++)
       {
           Point2f point = landmarks.at(i).at(j);
-          ellipse(frame, point, Size(10,10),0,0,0, Scalar(0,255,0),3);
+          ellipse(frame_gray, point, Size(10,10),0,0,0, Scalar(0,255,0),3);
       }
-
   }
-  //-- Show what you got
-  imshow( "Capture - Face detection", frame );
+  //-- Show image
+  imshow( "Capture - Face detection", frame_gray );
+  */
 }
+
+Face_worker::~Face_worker()
+{
+  delete facemark;
+}
+
 
 int main( int argc, char** argv )
 {
   ros::init(argc, argv, "face_detector_node");
   ros::NodeHandle n;
-
   ros::Rate loop_rate(100);
+
+  String face_classifier_filename = "insert_me";
+  String face_landmark_filename = "lbfmodel.yaml";
+  Face_worker faceworker;
+  faceworker.init(face_classifier_filename,face_landmark_filename);
 
 
   while (ros::ok()){
