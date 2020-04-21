695e9fa6bc559cbdc6eeba89b27b97c8def36e6a
ubuntu <vm@gmail.com>
2020-04-21 13:23:02 +0200

    small stuff, however seems like there is problems with server thingi

--- src/autonomous_eating/scripts/find_bowl.py
+++ src/autonomous_eating/scripts/find_bowl.py
@@ -11,7 +11,7 @@ from cv_bridge import CvBridge
 import image_geometry
 
 def deproject_func(x,y,z):
-    rospy.wait_for_service("deproject_pixel_to_world")
+    #rospy.wait_for_service("deproject_pixel_to_world", timeout=5)
     resp = deprojectResponse()
     try:
         service = rospy.ServiceProxy("deproject_pixel_to_world", deproject)
@@ -44,17 +44,17 @@ def depth_camera_callback(data): #converts the given image from camera into a CV
     except CvBridgeError as e:
         print(e)
 
-def cordinatecallback(data):  #recieves 3D cordinates and publishes them on /bowl_cords
-    global object_out_of_range
-    if data.data[2] !=0:
-        msg = Float32MultiArray()
-        array = [data.data[0], data.data[1], data.data[2]]
-        msg.data = array
-        pub_bowl.publish(msg)
-        object_out_of_range = False
-    else: 
+#def cordinatecallback(data):  #recieves 3D cordinates and publishes them on /bowl_cords
+ #   global object_out_of_range
+  #  if data.data[2] !=0:
+  #      msg = Float32MultiArray()
+  #      array = [data.data[0], data.data[1], data.data[2]]
+  #      msg.data = array
+  #      pub_bowl.publish(msg)
+  #      object_out_of_range = False
+  #  else: 
         #print "object is out of range"
-        object_out_of_range = True
+   #     object_out_of_range = True
     
 
 
@@ -127,13 +127,12 @@ pub_pixel = rospy.Publisher('/pixel_Cords', Float32MultiArray, queue_size=1)
 sub_bool = rospy.Subscriber("/find_bowl_trigger", Bool, bool_callback)
 color_sub = rospy.Subscriber("/r200/camera/color/image_raw", Image, color_camera_calback)
 depth_sub = rospy.Subscriber("/r200/camera/depth/image_raw", Image, depth_camera_callback)
-sub_cord3d = rospy.Subscriber("/3D_cordinates", Float32MultiArray, cordinatecallback)
+#sub_cord3d = rospy.Subscriber("/3D_cordinates", Float32MultiArray, cordinatecallback)
 
 debug = False   
-search = True
+search = False
 object_out_of_range = False 
 found_data = False
-UsingSimulatedCam = True
 
 data_color = None
 data_depth = None
@@ -167,6 +166,7 @@ if __name__ == '__main__':
            
             try: 
                 global c
+                
                 c = bowl_finder(bgr_image)          #tries to find bowl and gets center coordinates
                 d = depth_image[c]                      #gets depth
         
@@ -180,8 +180,9 @@ if __name__ == '__main__':
                 if debug == True:
                     print "did not find bowl"
                 
-            if ran == True and object_out_of_range == False : #if bowl was found, it publishes the camerafeed with marking of object                              
+            if ran == True: #if bowl was found, it publishes the camerafeed with marking of object                              
                 global biggest_contour
+                
                 image_to_publish = draw_square(bgr_image, biggest_contour)
                 image_msg = CvBridge().cv2_to_imgmsg(image_to_publish, "rgb8")
                 pub_img.publish(image_msg)
