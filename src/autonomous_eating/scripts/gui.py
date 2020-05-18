95152e597eb1dbf963673a4ec00c45149dd39e6d
HansLarsen <hans.larsen1994@gmail.com>
2020-05-18 18:04:56 +0200

    MoveStuffOutsideAfter

--- src/autonomous_eating/scripts/gui.py
+++ src/autonomous_eating/scripts/gui.py
@@ -24,22 +24,27 @@ pkgPath = rospkg.RosPack().get_path('autonomous_eating')
 class GUIApp():
   #callback for gui_mode
   def gui_mode_callback(self, data):
-    self.master.after(1,self.updateAcitvationBar(data.selectness))
+    self.selectness = data.selectness
+    #self.master.after(1,self.updateAcitvationBar(data.selectness))
     if data.y < 15:
-      self.itciY = data.y
+      self.itciY = data.y/20.0*600.0
     else:
-      self.itciY = data.y-10
-    self.master.after(1,self.updateItciImg(self.frame, data.x/10*350, self.itciY/20*600))
+      self.itciY = (data.y-10.0)/20.0*600.0
+
+    self.itciX = data.x/10*350
+
+    #self.master.after(1,self.updateItciImg(self.frame, data.x/10*350, self.itciY/20*600))
 
   #callback for gui_status
   def gui_status_callback(self, data):
     status = (data.jaco_status, data.camera_status, data.main_status, data.moving_status)
-    self.master.after(1,self.updateStatusText(status))
+    self.status = status
+    #self.master.after(1,self.updateStatusText(status))
 
   def gui_figure_callback(self, data):
     bridge = CvBridge()
     self.cv_img = bridge.imgmsg_to_cv2(data)
-    self.master.after(1, self.update_fig_task)
+    #self.master.after(1, self.update_fig_task)
 
   def find_face_trigger_callback(self, data):
     self.find_face_trigger = data.data
@@ -51,7 +56,7 @@ class GUIApp():
     if not self.find_face_trigger and not self.find_bowl_trigger:
       bridge = CvBridge()
       self.cv_img = bridge.imgmsg_to_cv2(data)
-      self.master.after(1, self.update_fig_task)
+      #self.master.after(1, self.update_fig_task)
 
   def update_fig_task(self):
     width, height, dim = self.cv_img.shape
@@ -61,21 +66,23 @@ class GUIApp():
     self.figureCanvas.create_image(0,0, anchor=NW, image=self.pil_img, state=NORMAL)
 
   #updates image of itci with the red circle showing tongue position
-  def updateItciImg(self, frame, x=25,y=25):
+  def updateItciImg(self): #frame, x=25,y=25):
     self.itciCanvas.create_image(0,0, anchor=NW, image=self.itciBaseImg, state=NORMAL)
     self.circleSize = 25
-    self.x1 = x - self.circleSize
-    self.x2 = x + self.circleSize
-    self.y1 = y - self.circleSize
-    self.y2 = y + self.circleSize
+    self.x1 = self.itciX - self.circleSize
+    self.x2 = self.itciX + self.circleSize
+    self.y1 = self.itciY - self.circleSize
+    self.y2 = self.itciY + self.circleSize
     self.itciCanvas.create_oval(self.x1,self.y1,self.x2,self.y2, width=5, outline="red")
+    self.activationBar['value'] = self.selectness
 
   #shows selectiveness as a status bar
-  def updateAcitvationBar(self, value=0):
-    self.activationBar['value'] = value
+  #def updateAcitvationBar(self):#, value=0):
+    #self.activationBar['value'] = self.selectness
 
   #updates text box with the status messages
-  def updateStatusText(self, status):
+  def updateStatusText(self):#, status):
+    status = self.status
     self.text.configure(state="normal") #unlock text field
     self.text.delete(1.0, END)
     self.text.insert(END, "JACO Connect:  " + status[0] + "\n")
@@ -93,6 +100,11 @@ class GUIApp():
     master.title("Autonomous Eating")
     self.master = master
 
+    self.itciY = 0
+    self.itciX = 0
+    self.status = ("Connected", "Disconnected", "OK", "False")
+    self.selectness = 0
+
     #bools for find_face and find_bowl triggers:
     self.find_bowl_trigger = False
     self.find_face_trigger = False
@@ -101,13 +113,13 @@ class GUIApp():
     self.text = Text(self.frame, font=("Arial", 12), width = 50)
     self.text.grid(column=0, row=0)
     # set text at start:
-    self.updateStatusText(("Connected", "Disconnected", "OK", "False"))
+    self.updateStatusText()
 
     #adding itci image with red ellipse showing tongue position
     self.itciBaseImg = ImageTk.PhotoImage(Image.open(pkgPath + "/extra/figures/itci2.png"))
     self.itciCanvas = Canvas(self.frame, width = self.itciBaseImg.width(), height = self.itciBaseImg.height())
     self.itciCanvas.grid(column=1, row=0, rowspan=2)
-    self.updateItciImg(self.frame) #center ellipse top-left at start
+    self.updateItciImg() #self.frame) #center ellipse top-left at start
 
     #add activationbar showing 'activationness' of the itci
     self.activationBar = Progressbar(self.frame, length=350)
@@ -135,4 +147,11 @@ app = GUIApp(root)
 #init ros:
 rospy.init_node('display_node')
 #loop using Tk instead of ros::spin, does the same thing, but keeps the GUI updating
-root.mainloop()
+while not rospy.is_shutdown():
+  app.update_fig_task()
+  #app.updateAcitvationBar()
+  app.updateItciImg()
+  app.updateStatusText()
+
+  root.update_idletasks()
+  root.update()
